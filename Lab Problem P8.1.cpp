3e355601afd5da4df70381c67f6839eee6c2252e
BoldOneKenobi <101661726+BoldOneKenobi@users.noreply.github.com>
2024-10-24 12:40:09 -0400

    Completed countCharacter function.

--- Lab Problem P8.1.cpp
+++ Lab Problem P8.1.cpp
@@ -1,3 +1,25 @@
+/*
+ * File: Lab Problem P8.1
+ * Author: Parker West
+ * Date: 10/24/2024
+ * Description: Program that counts the number of vowles in a string
+*/
+#include <iostream>
+#include <string>
+using namespace std; 
+
+//function prototypes
+int countCharacter(string str);
+
 int main() {
-	//hi
+	string str;
+	cout << "Enter a string or Q to quit: ";
+	getline(cin, str);
+	cout << "\nThe string length is: " << countCharacter(str);
+}
+
+int countCharacter(string str) {
+	int length = str.length();
+	return length;
+
 }
